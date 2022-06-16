0947488f33a74dc64728c1c309b66f4b65b2a87c
dannykhuu2 <dannykhuu2@gmail.com>
2022-06-15 23:44:24 -0400

    leetcode - complete permutations solution

---
+++ Java/Permutations.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        permuteHelper(list, new ArrayList<Integer>(), nums);
+        return list;
+    }
+    
+    private void permuteHelper(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            list.add(new ArrayList<Integer>(tempList));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                 if (!tempList.contains(nums[i])) {
+                     tempList.add(nums[i]);
+                     permuteHelper(list, tempList, nums);
+                     tempList.remove(tempList.size() - 1);
+                 }
+            }
+        }
+    }
+}

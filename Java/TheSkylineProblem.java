aac5aa15695e82e055981e86884007528b8f90b8
dannykhuu2 <dannykhuu2@gmail.com>
2022-06-27 00:36:16 -0400

    leetcode - the skyline problem, this version is easiest to understand and O(logn)

--- Java/TheSkylineProblem.java
+++ Java/TheSkylineProblem.java
@@ -2,12 +2,11 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
         int[][] coordinates = new int[2*buildings.length][3];
-        PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder());
+        TreeMap<Integer, Integer> queue = new TreeMap<>();
         List<List<Integer>> result = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
         int maxHeight = 0;
         int i = 0;
-        maxPQ.add(0);
+        queue.put(0, 1);
         
         for (int[] building : buildings) {
             coordinates[i][0] = building[0];
@@ -23,25 +22,31 @@ class Solution {
             if (a[0] != b[0]) {
                 return a[0] - b[0];
             } else {
-                return b[2] - a[2];
+                return (a[2] == 1 ? -a[1] : a[1]) - (b[2] == 1 ? -b[1] : b[1]);
             }
         });
         
         for (int[] coordinate : coordinates) {
             if (coordinate[2] == 1) {
-                maxPQ.add(coordinate[1]);
+                queue.compute(coordinate[1], (key, value) -> {
+                    if (value != null) {
+                        return value + 1;
+                    }
+                    return 1;
+                });
             } else {
-                maxPQ.remove(coordinate[1]);
+                queue.compute(coordinate[1], (key, value) -> {
+                    if (value == 1) {
+                        return null;
+                    }
+                    return value - 1;
+                });
             }
             
-            int currentHeight = maxPQ.peek();
+            int currentHeight = queue.lastKey();
             
             if (currentHeight != maxHeight) {
-                if (set.add(coordinate[0])) {
-                    result.add(Arrays.asList(coordinate[0], currentHeight));
-                } else {
-                    result.set(result.size() - 1, Arrays.asList(coordinate[0], currentHeight));
-                } 
+                result.add(Arrays.asList(coordinate[0], currentHeight));
                 maxHeight = currentHeight;
             }
         }

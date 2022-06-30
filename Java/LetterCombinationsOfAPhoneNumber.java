fd024c6a1c248f0e2916f649cb4491048d275788
dannykhuu2 <dannykhuu2@gmail.com>
2022-06-29 20:24:44 -0400

    leetcode - add recursive solution using string builder that has shorter runtime

--- Java/LetterCombinationsOfAPhoneNumber.java
+++ Java/LetterCombinationsOfAPhoneNumber.java
@@ -2,21 +2,47 @@ import java.util.*;
 class Solution {
     public List<String> letterCombinations(String digits) {
         String[] letters = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv" ,"wxyz"};
-        LinkedList<String> combinations = new LinkedList<>();
+        List<String> combinations = new ArrayList<>();
+        StringBuilder current = new StringBuilder();
         
         if (digits.length() == 0) return combinations;
-        combinations.add("");
-            
-        for (int i = 0; i < digits.length(); i++) {
-            int index = Character.getNumericValue(digits.charAt(i));
-            while (combinations.peek().length() == i) {
-                String current = combinations.poll();
-                for (int j = 0; j < letters[index].length(); j++) {
-                    combinations.add(current + letters[index].charAt(j));
-                }
-            }
-        }
-        
+        letterCombinationsHelper(digits, letters, combinations, 0, current);
         return combinations;
     }
+    
+    private void letterCombinationsHelper(String digits, String[] letters, List<String> combinations, int index, StringBuilder current) {
+        if (index == digits.length()) {
+            if (index != 0) combinations.add(current.toString());
+            return;
+        }
+        
+        int indexLetter = Character.getNumericValue(digits.charAt(index));
+        for (int i = 0; i < letters[indexLetter].length(); i++) {
+            current.append(letters[indexLetter].charAt(i));
+            letterCombinationsHelper(digits, letters, combinations, index + 1, current);
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
 }
+
+// class Solution {
+//     public List<String> letterCombinations(String digits) {
+//         String[] letters = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv" ,"wxyz"};
+//         LinkedList<String> combinations = new LinkedList<>();
+        
+//         if (digits.length() == 0) return combinations;
+//         combinations.add("");
+            
+//         for (int i = 0; i < digits.length(); i++) {
+//             int index = Character.getNumericValue(digits.charAt(i));
+//             while (combinations.peek().length() == i) {
+//                 String current = combinations.poll();
+//                 for (int j = 0; j < letters[index].length(); j++) {
+//                     combinations.add(current + letters[index].charAt(j));
+//                 }
+//             }
+//         }
+        
+//         return combinations;
+//     }
+// }

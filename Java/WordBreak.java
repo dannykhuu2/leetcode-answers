a20beb090165d334b37247f892034053655dee43
dannykhuu2 <dannykhuu2@gmail.com>
2022-07-25 00:37:12 -0400

    leetcode - cleanup

--- Java/WordBreak.java
+++ Java/WordBreak.java
@@ -2,12 +2,11 @@ import java.util.*;
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] chars = new boolean[s.length() + 1];
-        
         chars[0] = true;
         
         for (int i = 1; i <= s.length(); i++) {
-            for (String str: wordDict) { 
-                if (str.length() <= i && chars[i - str.length()] && s.substring(i-str.length(), i).equals(str)) {
+            for (String word : wordDict) {
+                if (word.length() <= i && chars[i - word.length()] && s.substring(i - word.length(), i).equals(word)) {
                     chars[i] = true;
                     break;
                 }

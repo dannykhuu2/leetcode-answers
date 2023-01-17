ea12515c1d823f84301eab22bf5a7a2737baf64e
dannykhuu2 <dannykhuu2@gmail.com>
2023-01-17 00:25:01 -0500

    python - complete gas stations leetcode solution

---
+++ Python/GasStations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        totalGas = 0
+        totalCost = 0
+        tank = 0
+        start = 0
+
+        for i in range(n):
+            totalGas += gas[i]
+            totalCost += cost[i]
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                start = i + 1
+                tank = 0
+        return -1 if totalCost > totalGas else start
